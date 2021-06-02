745044079fdbc81ea7ec91b693f986806e73e0d5
ankurk10 <59817633+ankurk10@users.noreply.github.com>
2021-06-02 20:04:31 +0530

    Added python files for Module-7

---
+++ Module7/multiply_num_in_list.py
@@ -0,0 +1,12 @@
+print("Ankur Kumar Kushwaha   1900300100034")
+
+list = [2, 3, 4, 5, 6]
+
+def multiplylist(list):
+  total = 1
+  for item in list:
+	  total = total * item
+
+  print(total)
+
+multiplylist(list)
---
+++ Module7/num_in_range.py
@@ -0,0 +1,13 @@
+print("Ankur Kumar Kushwaha   1900300100034")
+
+def checkrange(n) :
+   if n in range(2,10) :
+       print("Number present in the given range")
+
+   else :
+        print("Number not present in the given range")
+
+num = int(input("Enter an Integer: "))
+checkrange(num)
+
+
